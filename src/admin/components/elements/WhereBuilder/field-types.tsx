344887c5e01f6a66b1bb279a1570acc4115d5d6d
Bekir Yüksektepe <bekir@arkulpa.at>
2023-08-07 16:52:50 +0200

    add group support

--- src/admin/components/elements/WhereBuilder/field-types.tsx
+++ src/admin/components/elements/WhereBuilder/field-types.tsx
@@ -1,123 +1,127 @@
 const boolean = [
   {
-    label: 'equals',
-    value: 'equals',
+    label: "equals",
+    value: "equals",
   },
   {
-    label: 'isNotEqualTo',
-    value: 'not_equals',
+    label: "isNotEqualTo",
+    value: "not_equals",
   },
 ];
 
 const base = [
   ...boolean,
   {
-    label: 'isIn',
-    value: 'in',
+    label: "isIn",
+    value: "in",
   },
   {
-    label: 'isNotIn',
-    value: 'not_in',
+    label: "isNotIn",
+    value: "not_in",
   },
   {
-    label: 'exists',
-    value: 'exists',
+    label: "exists",
+    value: "exists",
   },
 ];
 
 const numeric = [
   ...base,
   {
-    label: 'isGreaterThan',
-    value: 'greater_than',
+    label: "isGreaterThan",
+    value: "greater_than",
   },
   {
-    label: 'isLessThan',
-    value: 'less_than',
+    label: "isLessThan",
+    value: "less_than",
   },
   {
-    label: 'isLessThanOrEqualTo',
-    value: 'less_than_equal',
+    label: "isLessThanOrEqualTo",
+    value: "less_than_equal",
   },
   {
-    label: 'isGreaterThanOrEqualTo',
-    value: 'greater_than_equals',
+    label: "isGreaterThanOrEqualTo",
+    value: "greater_than_equals",
   },
 ];
 
 const geo = [
   ...boolean,
   {
-    label: 'exists',
-    value: 'exists',
+    label: "exists",
+    value: "exists",
   },
   {
-    label: 'near',
-    value: 'near',
+    label: "near",
+    value: "near",
   },
 ];
 
 const like = {
-  label: 'isLike',
-  value: 'like',
+  label: "isLike",
+  value: "like",
 };
 
 const contains = {
-  label: 'contains',
-  value: 'contains',
+  label: "contains",
+  value: "contains",
 };
 
 const fieldTypeConditions = {
   text: {
-    component: 'Text',
+    component: "Text",
+    operators: [...base, like, contains],
+  },
+  group: {
+    component: "Group",
     operators: [...base, like, contains],
   },
   email: {
-    component: 'Text',
+    component: "Text",
     operators: [...base, contains],
   },
   textarea: {
-    component: 'Text',
+    component: "Text",
     operators: [...base, like, contains],
   },
   code: {
-    component: 'Text',
+    component: "Text",
     operators: [...base, like, contains],
   },
   json: {
-    component: 'Text',
+    component: "Text",
     operators: [...base, like, contains],
   },
   richText: {
-    component: 'Text',
+    component: "Text",
     operators: [...base, like, contains],
   },
   number: {
-    component: 'Number',
+    component: "Number",
     operators: [...base, ...numeric],
   },
   date: {
-    component: 'Date',
+    component: "Date",
     operators: [...base, ...numeric],
   },
   point: {
-    component: 'Point',
+    component: "Point",
     operators: [...geo],
   },
   upload: {
-    component: 'Text',
+    component: "Text",
     operators: [...base],
   },
   relationship: {
-    component: 'Relationship',
+    component: "Relationship",
     operators: [...base],
   },
   select: {
-    component: 'Text',
+    component: "Text",
     operators: [...base],
   },
   checkbox: {
-    component: 'Text',
+    component: "Text",
     operators: boolean,
   },
 };
